f4657604bac567224e8f56cc22bd463d5adf3785
AnafaHyams <anafa18@gmail.com>
2018-08-03 21:28:50 +0300

    update take photo from camera and upload photo from folder and show the selected photo.

--- app/src/main/java/com/example/anafa/wearit/UserSearchByPhotoActivity.java
+++ app/src/main/java/com/example/anafa/wearit/UserSearchByPhotoActivity.java
@@ -1,7 +1,9 @@
 package com.example.anafa.wearit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.widget.RadioButton;
 
 public class UserSearchByPhotoActivity extends AppCompatActivity {
 
+    private static final Integer REQUEST_CAMERA = 0;
+    private static final Integer SELECT_FILE = 1;
     ImageView dynamicImageView;
 
     @Override
@@ -19,13 +23,25 @@ public class UserSearchByPhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_search_by_photo);
 
         RadioButton cameraRadioButton = (RadioButton) findViewById(R.id.takePictureCheckBox);
+        RadioButton openPhotoFromFileRadioButton = (RadioButton) findViewById(R.id.uploadImageCheckBox);
         dynamicImageView = (ImageView) findViewById(R.id.dynamicPhotoImageView);
 
+        // Take photo from camera
         cameraRadioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                startActivityForResult(intent, REQUEST_CAMERA);
+            }
+        });
+
+        //Open image from file at device
+        openPhotoFromFileRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
             }
         });
     }
@@ -33,10 +49,16 @@ public class UserSearchByPhotoActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
 
-        dynamicImageView.setImageBitmap(bitmap);
+        if (resultCode == Activity.RESULT_OK)
+        {
+            if (requestCode == REQUEST_CAMERA){
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                dynamicImageView.setImageBitmap(bitmap);
+            } else if(requestCode == SELECT_FILE){
+                Uri selectImageUri = data.getData();
+                dynamicImageView.setImageURI(selectImageUri);
+            }
+        }
     }
-
-
 }
