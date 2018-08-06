5f857011d60f8113070d5d28d251271ebec4abbe
AnafaHyams <anafa18@gmail.com>
2018-08-06 20:14:02 +0300

    search text user entered

--- app/src/main/java/com/example/anafa/wearit/UserSearchByTextActivity.java
+++ app/src/main/java/com/example/anafa/wearit/UserSearchByTextActivity.java
@@ -19,13 +19,14 @@ public class UserSearchByTextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search_by_text);
 
-        TextView text = (TextView) findViewById(R.id.textToSearch);
+        final TextView text = (TextView) findViewById(R.id.textToSearch);
         Button searchBtn = (Button) findViewById(R.id.searchButton);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "https://www.google.co.il/search?&q=dog&oq=dog&userip=" + getUserIPAddress();
+                String txtToSearch = text.getText().toString();
+                String url = "https://www.google.co.il/search?&q=" + txtToSearch + "&oq=" + txtToSearch + "&userip=" + getUserIPAddress();
                 // {google:baseURL}search?q=%s&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:iOSSearchLanguage}{google:searchClient}{google:sourceId}{google:contextualSearchVersion}ie={inputEncoding}
                 WebView webview = (WebView) findViewById(R.id.myWebView);
                 webview.setWebViewClient(new WebViewClient());
