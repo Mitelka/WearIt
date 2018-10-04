06075bc194e97f4fcea7bab04d57b3b66b637bf0
mitelka2013@gmail.com <mitelka2013@gmail.com>
2018-10-04 19:46:33 +0300

    Upload UI class

---
+++ app/src/main/java/com/example/anafa/wearit/UI.java
@@ -0,0 +1,14 @@
+package com.example.anafa.wearit;
+
+import android.app.Activity;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class UI {
+
+    public void showResults(GridView gridView, Activity activity, ArrayList itemList, int showResultsAt) {
+        CustomArrayListAdapter myAdapter = new CustomArrayListAdapter(activity, showResultsAt, itemList);
+        gridView.setAdapter(myAdapter);
+    }
+}
