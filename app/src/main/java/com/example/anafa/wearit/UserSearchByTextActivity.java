0593d6d4c4ff65c717dcd44c18807f095e6e3714
AnafaHyams <anafa18@gmail.com>
2018-10-12 02:44:32 +0300

    show results sorted by price and by stars on SEARCH BY Text --> Need to throw same functions out of activities (photo&text activity)

--- app/src/main/java/com/example/anafa/wearit/UserSearchByTextActivity.java
+++ app/src/main/java/com/example/anafa/wearit/UserSearchByTextActivity.java
@@ -9,22 +9,22 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 
-public class UserSearchByTextActivity extends AppCompatActivity
-{
+public class UserSearchByTextActivity extends AppCompatActivity {
     private static final String EMPTY_STRING = "";
     public static final int List_View_Type = 2;
     private PropertyReader propertyReader;
     private GoogleSearch googleSearch;
     private ServerConnector serverConnector;
+    TabHost tabHost;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search_by_text);
 
@@ -36,52 +36,70 @@ public class UserSearchByTextActivity extends AppCompatActivity
         final TextView text = (TextView) findViewById(R.id.textToSearch);
         Button searchBtn = (Button) findViewById(R.id.searchButton);
 
-        searchBtn.setOnClickListener(new View.OnClickListener()
-        {
+        //TODO: Throw to other activity --> the same as at Search By Photo
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setVisibility(View.INVISIBLE);
+        tabHost.setup();
+
+        //tab1
+        TabHost.TabSpec spec = tabHost.newTabSpec("Price");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("SORTED BY PRICE");
+        tabHost.addTab(spec);
+
+        //tab2
+        spec = tabHost.newTabSpec("tab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("SORTED BY STARS");
+        tabHost.addTab(spec);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String txtToSearch = text.getText().toString();
-                Toast.makeText(UserSearchByTextActivity.this, "product name to search: " + txtToSearch , Toast.LENGTH_LONG).show();
-                if(!txtToSearch.equals(EMPTY_STRING))
-                {
+                Toast.makeText(UserSearchByTextActivity.this, "product name to search: " + txtToSearch, Toast.LENGTH_LONG).show();
+                if (!txtToSearch.equals(EMPTY_STRING)) {
                     searchTextAtGoogle(txtToSearch);
-                }
-                else
-                {
-                    Toast.makeText(UserSearchByTextActivity.this, "You didn't entered product name to search" , Toast.LENGTH_LONG).show();
+                    tabHost.setVisibility(View.VISIBLE);
+                } else {
+                    Toast.makeText(UserSearchByTextActivity.this, "You didn't entered product name to search", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
-    private void searchTextAtGoogle(String txtToSearch)
-    {
+    private void searchTextAtGoogle(String txtToSearch) {
         InputMethodManager inputManager = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
         inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
-        try
-        {
+        try {
             UI ui = new UI();
             ArrayList itemListToShow;
 
-            // Get results item list list from server
-            itemListToShow= ui.genericSearchByText(txtToSearch, propertyReader);
-            showResultsOfSearch(itemListToShow);
-        }
-        catch (Exception e)
-        {
+            // Get results item list from server
+            ArrayList itemListsToShow = ui.genericSearchByText(txtToSearch, propertyReader);
+            ArrayList itemListToShowByPrice = (ArrayList) itemListsToShow.get(0);
+            ArrayList itemListToShowByStars = (ArrayList) itemListsToShow.get(1);
+
+            showResultsOfSearch(itemListToShowByPrice, itemListToShowByStars);
+        } catch (Exception e) {
             Toast toast = Toast.makeText(this, "Cannot connect googleSearch", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
 
-    private void showResultsOfSearch(ArrayList itemListToShow)
-    {
-        ListView listView = (ListView) findViewById(R.id.ResultsListView);
+    private void showResultsOfSearch(ArrayList itemListToShowByPrice, ArrayList itemListToShowByStars) {
+        ListView priceListView = (ListView) findViewById(R.id.ResultsByPriceListView);
+        ListView starsListView = (ListView) findViewById(R.id.ResultsByStarsListView);
 
         //type=2-->ListView
-        UI.showResults(listView, this, itemListToShow, R.layout.content_list_view_results, List_View_Type);
-        addClickListener(listView, itemListToShow);
+        // Sorted by price
+        UI.showResults(priceListView, this, itemListToShowByPrice, R.layout.content_list_view_results, List_View_Type);
+        addClickListener(priceListView, itemListToShowByPrice);
+
+        // Sorted by stars
+        UI.showResults(starsListView, this, itemListToShowByStars, R.layout.content_list_view_results, List_View_Type);
+        addClickListener(starsListView, itemListToShowByStars);
     }
 
     private void addClickListener(ListView listView, final ArrayList itemListToShow) {
