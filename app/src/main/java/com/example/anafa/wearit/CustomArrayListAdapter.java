d91edbd15b41b79268ea05473af7e6925717141b
AnafaHyams <anafa18@gmail.com>
2018-10-11 17:46:57 +0300

    Adding image from web site for each item result

--- app/src/main/java/com/example/anafa/wearit/CustomArrayListAdapter.java
+++ app/src/main/java/com/example/anafa/wearit/CustomArrayListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.anafa.wearit;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +10,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class CustomArrayListAdapter extends ArrayAdapter {
@@ -39,21 +44,32 @@ public class CustomArrayListAdapter extends ArrayAdapter {
         TextView itemLinkTextView = (TextView) convertView.findViewById(R.id.itemLinkTextView);
 
         itemNameTextView.setText(itemList.get(position).getItemListName());
-        //itemImageView.setImageResource(itemList.get(position).getItemListImage());
         itemPriceTextView.setText(itemList.get(position).getItemListPrice());
         itemLinkTextView.setText(itemList.get(position).getItemListLink());
 
+        Bitmap bitmap = getBitmapFromURL(itemList.get(position).getItemListImage());
+        itemImageView.setImageBitmap(bitmap);
 
         TextView itemStarsTextView = (TextView) convertView.findViewById(R.id.itemStarsTextView);
         itemStarsTextView.setText(itemList.get(position).getItemListStars() + "");
 
-//        if(itemList.get(position).getItemListStars() != 0){
-//            TextView itemStarsTextView = (TextView) convertView.findViewById(R.id.itemStarsTextView);
-//            itemStarsTextView.setText(itemList.get(position).getItemListStars() + "");
-//        } else {
-//            TextView itemStarsTextView = (TextView) convertView.findViewById(R.id.itemStarsTextView);
-//            itemStarsTextView.setText(itemList.get(position).getItemListStars() + "");
-//        }
         return convertView;
+    }
+
+    private Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
